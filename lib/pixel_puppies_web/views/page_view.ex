cad1e25265b157bbbaa515dd53c756e4c465b421
Hez Ronningen <code@hez.ca>
2019-07-19 08:48:19 -0700

    alphanumerically sort pics and add title

--- lib/pixel_puppies_web/views/page_view.ex
+++ lib/pixel_puppies_web/views/page_view.ex
@@ -3,13 +3,17 @@ defmodule PixelPuppiesWeb.PageView do
 
   @pet_path "/images/pets/"
   @pets_directory "assets/static/images/pets"
-  @file_ext [".jpg", "jpeg", ".png", "gif"]
+  @file_ext [".jpg", ".jpeg", ".png", ".gif"]
 
   def all_pet_files do
     @pets_directory
     |> File.ls!()
+    |> Enum.sort()
+    |> Enum.reverse()
     |> Enum.filter(&String.ends_with?(&1, @file_ext))
   end
+
+  def pet_pic_title(file), do: String.replace(file, @file_ext, "")
 
   def pet_path(pic), do: @pet_path <> pic
 end
